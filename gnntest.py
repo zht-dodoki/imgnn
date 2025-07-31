ffda318c8f5cef592ba20a63b68fdda014dde954
Z.h.t <134757647+zht-dodoki@users.noreply.github.com>
2025-08-01 03:26:33 +0800

    Update gnntest.py

--- gnntest.py
+++ gnntest.py
@@ -3,18 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class GNN(nn.Module):
-    def __init__(self, in_features, hidden_features1, hidden_features2, out_features, dropout=0.5, device='cpu'):
+    def __init__(self, in_features, hidden_features1, hidden_features2, out_features, dropout=0.5):
         super(GNN, self).__init__()
-        self.gc1 = GraphConvolution(in_features, hidden_features1, device=device)
-        self.gc2 = GraphConvolution(hidden_features1, hidden_features2, device=device)
-        self.gc3 = GraphConvolution(hidden_features2, out_features, device=device)
+        self.gc1 = GraphConvolution(in_features, hidden_features1)
+        self.gc2 = GraphConvolution(hidden_features1, hidden_features2)
+        self.gc3 = GraphConvolution(hidden_features2, out_features)
         self.fc = nn.Linear(out_features, 1)
         self.dropout = dropout
-        self.device = device
+
 
     def forward(self, x, adj):
-        x = x.to(self.device)
-        adj = adj.to(self.device)
         x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
         x = F.relu(self.gc2(x, adj))
@@ -26,7 +24,7 @@ class GNN(nn.Module):
         return x
 
 class GraphConvolution(nn.Module):
-    def __init__(self, in_features, out_features, bias=True, device='cpu'):
+    def __init__(self, in_features, out_features, bias=True):
         super(GraphConvolution, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -36,7 +34,7 @@ class GraphConvolution(nn.Module):
         else:
             self.register_parameter('bias', None)
         self.reset_parameters()
-        self.device = device
+
 
     def reset_parameters(self):
         stdv = 1. / self.weight.size(1) ** 0.5
@@ -45,8 +43,6 @@ class GraphConvolution(nn.Module):
             self.bias.data.uniform_(-stdv, stdv)
 
     def forward(self, input, adj):
-        input = input.to(self.device)
-        adj = adj.to(self.device)
         support = torch.mm(input, self.weight)
         output = torch.spmm(adj, support)
         if self.bias is not None:
